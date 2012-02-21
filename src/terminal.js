e3d9773445bcb363ce31c1192467ab97cfae6e80
C Lanam <clanam@yahoo-inc.com>
2012-02-21 15:49:38 -0800

    Fixed a fix again.

--- src/terminal.js
+++ src/terminal.js
@@ -36,7 +36,9 @@ $terminal.bind('keydown', function(e) {
         letter;
 
     // if I don't do this, ctrl+r page reloads are getting munched
-    if (e.ctrlKey || key === 91 /*ctrl-chrome*/ || key === 224 /*ctrl-ff*/) {
+    if (isCtrl) {
+        return;
+    } else if (e.ctrlKey || key === 91 /*ctrl-chrome*/ || key === 224 /*ctrl-ff*/) {
         isCtrl = true;
         return;
     } else if (isCtrl && key === 16 /*shift*/) {
